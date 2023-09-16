a902f036647646ff07271bd4092ce2b31080382f
Sergio <sergiorobayorr@gmail.com>
2023-09-16 07:57:46 -0500

    Modeling the user state with redux toolkit

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,14 +4,14 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="font-pizza flex justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+    <header className="font-pizza flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
 
       <SearchOrder />
 
-      {/* <Username /> */}
+      <Username />
     </header>
   );
 }
